5cc40203e1016d66a090b23ae90158ceedad48a8
Steven Hall <Hallzy.18@gmail.com>
2018-08-07 21:09:06 -0700

    Greatly simplified the sql querying

--- pickapart.php
+++ pickapart.php
@@ -27,7 +27,8 @@ echo " URLs use the \"%\" sign to denote special characters, and it so happens";
 echo " that \"%25\" is the code for a \"%\" sign";
 echo "<br>";
 echo "<br>";
-echo "The tables to  query from are \"cars\", \"pics\", and \"lot\"";
+echo "The tables to  query from are \"cars\", \"pics\", and \"lot\",";
+echo "\"on_sale_items\", \"on_sale_date\"";
 echo "<br>";
 echo "For example, the default query is:";
 echo "<br>";
@@ -41,14 +42,6 @@ echo "<br>";
 $username = "stmhallc_user";
 $password = "04otmJr2K";
 $db = "stmhallc_cars";
-$servername = "localhost";
-
-$con=mysqli_connect($servername, $username, $password, $db);
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
 
 $query = $_GET['query'];
 
@@ -60,111 +53,11 @@ if ($query == '') {
 echo "<br>";
 echo "Your Query: <strong>$query</strong>";
 
-$result = mysqli_query($con,$query);
-
-$num_rows = mysqli_num_rows($result);
-
-echo "<br>";
-if ($num_rows != false) {
-  echo "Which resulted in <strong>$num_rows</strong> rows.";
-echo "<br>";
-  echo "<a href=\"pickapart_table.php?query=$query\">Table</a>";
-  echo "&nbsp;&nbsp;&nbsp;";
-  echo "<a href=\"pickapart_json.php?query=$query\">JSON</a>";
-  echo "&nbsp;&nbsp;&nbsp;";
-  echo "<a href=\"pickapart_csv.php?query=$query\">CSV</a>";
-}
-else {
-  echo "Query <strong>Failed</strong>";
-}
-
-
-/* Need to get the names of the headings */
-/* If we are looking for * then we don't care */
-/* Make the query lowercase for easier parsing */
-$mystring = strtolower($query);
-if (strpos($mystring, " * ") == false) {
-  /* Remove everything after "from" */
-  $mystring = explode(" from", $mystring);
-  /* Remove "select" */
-  $mystring = explode("select", $mystring[0]);
-
-  $mystring = $mystring[1];
-  /* split by commas, if they exist */
-  if (strpos($mystring, ",") != false) {
-      $mystring = explode(",", $mystring);
-  }
-
-
-  /* We now have an array of each element being selected. Just need to check */
-  /* if they have the "as" keyword */
-  for ($i=0; $i<count($mystring); $i++) {
-    /* split by commas, if they exist */
-    if (count($mystring) == 1) {
-      if (strpos($mystring, "as") != false) {
-        $mystring = explode("as ", $mystring);
-        $mystring = $mystring[1];
-      }
-    }
-    else {
-      if (strpos($mystring[$i], "as") != false) {
-        $mystring[$i] = explode("as ", $mystring[$i]);
-        $mystring[$i] = $mystring[$i][1];
-      }
-    }
-  }
-}
-else {
-  $mystring = array("urls", "date_added", "make", "model", "year",
-                    "body_style", "engine", "transmission", "description",
-                    "row", "stock");
-
-}
-
-
-echo "<table border='1'><tr>";
-for ($i=0; $i<count($mystring); $i++) {
-  if (count($mystring) == 1) {
-    $label = $mystring;
-  }
-  else {
-    $label = $mystring[$i];
-  }
-  $label = str_replace (' ', '', $label);
-  echo "<th>$label</th>";
-}
-echo "</tr>";
-
-while ($row    = mysqli_fetch_assoc($result)) {
-  echo "<tr>";
-  for ($i=0; $i<count($mystring); $i++) {
-    if (count($mystring) == 1) {
-      $label = $mystring;
-    }
-    else {
-      $label = $mystring[$i];
-    }
-    $label = str_replace (' ', '', $label);
-    /* If is the URLs section then we want to convert the URLs into links */
-    if ($label == "urls" && $row[$label] != "") {
-      $urls = explode(" ", $row[$label]);
-      for ($j=0; $j<count($urls); $j++) {
-        $x = $j + 1;
-        $urls[$j] = "<a href=\"" . $urls[$j] . "\">" . $x . ".jpg</a>";
-      }
-      $url_list = implode(", ", $urls);
-      echo "<td>" . $url_list . "</td>";
-    }
-    else {
-      echo "<td>" . $row[$label] . "</td>";
-    }
-  }
-  echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($con);
-
-
-
-?>
+// Decode because it could have html we want to render. Such as anchor tags for
+// image links
+echo html_entity_decode(shell_exec("
+    mysql --html -u '$username' -p'$password' << EOF
+    use $db;
+    $query;
+EOF
+  "));
